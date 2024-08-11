3bbff2e6cd33c76f7c6f2dd245a69432351e7d78
Gaurav Patil <gauravpatil7009@gmail.com>
2024-08-11 23:50:59 +0530

    Revised Code

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,68 +1,115 @@
-import { Container, Flex, Link, Skeleton, SkeletonCircle, Text, VStack } from "@chakra-ui/react";
+import {
+  Container,
+  Flex,
+  Link,
+  Skeleton,
+  SkeletonCircle,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import ProfileHeader from "../../components/Profile/ProfileHeader";
 import ProfileTabs from "../../components/Profile/ProfileTabs";
 import ProfilePosts from "../../components/Profile/ProfilePosts";
 import useGetUserProfileByUsername from "../../hooks/useGetUserProfileByUsername";
 import { useParams } from "react-router-dom";
 import { Link as RouterLink } from "react-router-dom";
+import { useState } from "react";
+import SavedPostsOrReels from "../../components/Profile/SavedPostsOrReels";
+import LikedPostsOrReels from "../../components/Profile/LikedPostsOrReels";
 
 const ProfilePage = () => {
-	const { username } = useParams();
-	const { isLoading, userProfile } = useGetUserProfileByUsername(username);
+  const { username } = useParams();
+  const { isLoading, userProfile } = useGetUserProfileByUsername(username);
+  const [activeTab, setActiveTab] = useState("posts");
 
-	const userNotFound = !isLoading && !userProfile;
-	if (userNotFound) return <UserNotFound />;
+  const userNotFound = !isLoading && !userProfile;
+  if (userNotFound) return <UserNotFound />;
 
-	return (
-		<Container maxW='container.lg' py={5}>
-			<Flex py={10} px={4} pl={{ base: 4, md: 10 }} w={"full"} mx={"auto"} flexDirection={"column"}>
-				{!isLoading && userProfile && <ProfileHeader />}
-				{isLoading && <ProfileHeaderSkeleton />}
-			</Flex>
-			<Flex
-				px={{ base: 2, sm: 4 }}
-				maxW={"full"}
-				mx={"auto"}
-				borderTop={"1px solid"}
-				borderColor={"whiteAlpha.300"}
-				direction={"column"}
-			>
-				<ProfileTabs />
-				<ProfilePosts />
-			</Flex>
-		</Container>
-	);
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case "posts":
+        return <ProfilePosts />;
+
+      case "reels":
+        return null;
+
+      case "saved":
+        return <SavedPostsOrReels />;
+      case "likes":
+        return <LikedPostsOrReels />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Container maxW="container.lg" py={5}>
+      <Flex
+        py={10}
+        px={4}
+        pl={{ base: 4, md: 10 }}
+        w={"full"}
+        mx={"auto"}
+        flexDirection={"column"}
+      >
+        {!isLoading && userProfile && <ProfileHeader />}
+        {isLoading && <ProfileHeaderSkeleton />}
+      </Flex>
+      <Flex
+        px={{ base: 2, sm: 4 }}
+        maxW={"full"}
+        mx={"auto"}
+        borderTop={"1px solid"}
+        borderColor={"whiteAlpha.300"}
+        direction={"column"}
+      >
+        <ProfileTabs activeTab={activeTab} onTabChange={setActiveTab} />
+        {renderTabContent()}
+      </Flex>
+    </Container>
+  );
 };
 
 export default ProfilePage;
 
 // skeleton for profile header
 const ProfileHeaderSkeleton = () => {
-	return (
-		<Flex
-			gap={{ base: 4, sm: 10 }}
-			py={10}
-			direction={{ base: "column", sm: "row" }}
-			justifyContent={"center"}
-			alignItems={"center"}
-		>
-			<SkeletonCircle size='24' />
+  return (
+    <Flex
+      gap={{ base: 4, sm: 10 }}
+      py={10}
+      direction={{ base: "column", sm: "row" }}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <SkeletonCircle size="24" />
 
-			<VStack alignItems={{ base: "center", sm: "flex-start" }} gap={2} mx={"auto"} flex={1}>
-				<Skeleton height='12px' width='150px' />
-				<Skeleton height='12px' width='100px' />
-			</VStack>
-		</Flex>
-	);
+      <VStack
+        alignItems={{ base: "center", sm: "flex-start" }}
+        gap={2}
+        mx={"auto"}
+        flex={1}
+      >
+        <Skeleton height="12px" width="150px" />
+        <Skeleton height="12px" width="100px" />
+      </VStack>
+    </Flex>
+  );
 };
 
 const UserNotFound = () => {
-	return (
-		<Flex flexDir='column' textAlign={"center"} mx={"auto"}>
-			<Text fontSize={"2xl"}>User Not Found</Text>
-			<Link as={RouterLink} to={"/"} color={"blue.500"} w={"max-content"} mx={"auto"}>
-				Go home
-			</Link>
-		</Flex>
-	);
+  return (
+    <Flex flexDir="column" textAlign={"center"} mx={"auto"}>
+      <Text fontSize={"2xl"}>User Not Found</Text>
+      <Link
+        as={RouterLink}
+        to={"/"}
+        color={"blue.500"}
+        w={"max-content"}
+        mx={"auto"}
+      >
+        Go home
+      </Link>
+    </Flex>
+  );
 };
