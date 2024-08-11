6afb7d6062e00875a3933b2c29d43db811e12488
Gaurav Patil <gauravpatil7009@gmail.com>
2024-08-11 23:50:27 +0530

    Updated Code to accept the Pin of reel change

--- src/pages/SavedPost/SavedPostPage.jsx
+++ src/pages/SavedPost/SavedPostPage.jsx
@@ -5,10 +5,12 @@ import { savedItems } from "../../components/Profile/SavedPostsOrReels";
 import { MdArrowBackIosNew } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
 import useAuthStore from "../../store/authStore";
+import OpenSavedReels from "../../components/Profile/OpenSavedReels";
 
 const SavedPostPage = () => {
   const [savedReels, setSavedReels] = useState([]);
-    const navigate = useNavigate();
+  const [pinnedReelId, setPinnedReelId] = useState(null);
+  const navigate = useNavigate();
 	const authUser = useAuthStore((state) => state.user);
 
   useEffect(() => {
@@ -19,6 +21,9 @@ const SavedPostPage = () => {
   const BacktoSavedPost = ()=>{
     navigate(`/${authUser?.username}`);
 }
+  const pinReel = (reelId) => {
+    setPinnedReelId(reelId);
+  };
   return (
     <>
     <Box py={5}borderRadius="lg" px={175} cursor="pointer" overflow="hidden">
@@ -30,13 +35,8 @@ const SavedPostPage = () => {
     </Box>
       <Grid templateColumns="repeat(3, 300px)" gap={2}>
         {savedReels.map((reel) => (
-          <Box key={reel.id} textAlign="center">
-            <Image
-              src={reel.imageUrl}
-              alt={reel.description}
-              boxSize="300px"
-              objectFit="cover"
-            />
+          <Box key={reel.id} textAlign="center" cursor="pointer">
+           < OpenSavedReels reel={reel} key={reel.id} pinReel={pinReel}/>
           </Box>
         ))}
       </Grid>
